0723b4c96fa7c2af401521238c8aa93333163fe2
“adiletD” <daniiarov.adilet@gmail.com>
2021-11-10 23:43:09 +0800

    rough initial setup of db schemas

--- services/menu/app.py
+++ services/menu/app.py
@@ -41,3 +41,60 @@ def get_db():
 @app.route('/')
 def hello():
     return "Hello bro!"
+
+# GET /students/<student_id> endpoint - returns the info about an indicated student
+
+
+@app.route('/stores/<store_id>/menus', methods=['GET'])
+def getMenu(store_id):
+
+    # call the stores
+    db = get_db()
+
+    menus = db.menu.find(
+        {'store_id': store_id})
+
+    if menus:
+        result = menus
+    else:
+        return jsonify(ERR_CODE), 404
+    return jsonify(result), 200
+
+
+@app.route('/stores/<store_id>/menus', methods=['PUT'])
+def uploadMenu(store_id):
+    db = get_db()
+    # check if the student already exists
+    student_id = request.get_json().get('student_id')
+    student = db.student.find_one({'student_id': student_id})
+    if student:
+        return jsonify("Student already exists"), 409
+    # if the stud does not exist then add him
+    else:
+        dept_name = request.get_json().get("dept_name")
+        gpa = request.get_json().get("gpa")
+        name = request.get_json().get("name")
+        student_id = request.get_json().get("student_id")
+
+        result = [{"dept_name": dept_name, "gpa": gpa,
+                  "name": name, "student_id": student_id}]
+
+        db.student.insert_one(result[0])
+
+    return jsonify("Success!"), 200
+
+
+@app.route('/takes/<student_id>', methods=['GET'])
+def getStudentsCoursesById(student_id):
+    db = get_db()
+    student = db.student.find_one({'student_id': student_id})
+    if student:
+        _courses = db.takes.find(
+            {'student_id': student["student_id"]}).sort('course_id', 1)
+        courses = [{"course_id": course["course_id"],
+                    "credits":course["credits"]} for course in _courses]
+        result = [{"dept_name": student["dept_name"], "gpa": student["gpa"],
+                   "name": student["name"], "student_id": student["student_id"], "student_takes": courses}]
+        return jsonify(result), 200
+    else:
+        return jsonify(ERR_CODE), 404
--- services/menu/init-db.js
+++ services/menu/init-db.js
@@ -1,25 +1,141 @@
 db.auth('comp3122', '12345')
-db = db.getSiblingDB('testdb')
+db = db.getSiblingDB('menu')
 
-db.createCollection('student')
+db.createCollection('item', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['id', 'title', 'price'],
+      properties: {
+        id: {
+          bsonType: 'string',
+          uniqueItems: true,
+          description: 'Must be unique string and is required',
+        },
+        description: {
+          bsonType: 'string',
+          description:
+            '(optional) Supplementary information describing the item (by locale).',
+        },
+        title: {
+          bsonType: 'string',
+          description: 'The name of the item (by locale).',
+        },
+        price: {
+          bsonType: 'in',
+          description: 'City; Required',
+        },
+      },
+    },
+  },
+})
 
-db.student.insertOne({
-  student_id: '33333',
-  name: 'Alice',
-  dept_name: 'Comp. Sci.',
-  gpa: 3.1,
+db.createCollection('menu', {
+  validator: {
+    $jsonSchema: {
+      bsonType: 'object',
+      required: ['id', 'title', 'price'],
+      properties: {
+        id: {
+          bsonType: 'string',
+          uniqueItems: true,
+          description: 'Must be unique string and is required',
+        },
+        title: {
+          bsonType: 'string',
+          description: 'The name of the men (by locale).',
+        },
+        subtitle: {
+          bsonType: 'string',
+          description: 'An optional subtitle for the menu (by locale).',
+        },
+        // service_availablity: {
+        //   bsonType: "object",
+        //   required: [],
+        //   properties:{
+        //     day_of_week: "",
+        //   }
+        // },
+      },
+    },
+  },
 })
-db.student.insertOne({
-  student_id: '22222',
-  name: 'Bob',
-  dept_name: 'History.',
-  gpa: 2.0,
+
+db.createCollection('category', {
+  validator: {
+    $jsoSchema: {
+      bsonType: 'object',
+      required: [],
+      properties: {
+        id: {
+          bsonType: 'string',
+          description: 'Must be unique string and is required',
+        },
+        entities: {
+          bsonType: ['array'],
+          items: {
+            bsonType: 'object',
+            required: ['type', 'id'],
+            properties: {
+              id: {
+                bsonType: 'string',
+                description: 'must be a string and is required',
+              },
+              type: {
+                bsonType: 'string',
+                description: 'must be a string and is required',
+              },
+            },
+          },
+          description: 'array of items ',
+        },
+        title: {
+          bsonType: 'string',
+          description: 'The name of the men (by locale).',
+        },
+      },
+    },
+  },
 })
-db.student.insertOne({
-  student_id: '11111',
-  name: 'Carol',
-  dept_name: 'History',
-  gpa: 2.1,
+
+db.menu.insertOne({
+  id: 'empty_menu_id',
+  title: {
+    translations: {
+      en_us: 'Empty Menu',
+    },
+  },
+  service_availability: [
+    {
+      day_of_week: 'monday',
+      time_periods: [{ start_time: '00:00', end_time: '23:59' }],
+    },
+    {
+      day_of_week: 'tuesday',
+      time_periods: [{ start_time: '00:00', end_time: '23:59' }],
+    },
+    {
+      day_of_week: 'wednesday',
+      time_periods: [{ start_time: '00:00', end_time: '23:59' }],
+    },
+    {
+      day_of_week: 'thursday',
+      time_periods: [{ start_time: '00:00', end_time: '23:59' }],
+    },
+    {
+      day_of_week: 'friday',
+      time_periods: [{ start_time: '00:00', end_time: '23:59' }],
+    },
+    {
+      day_of_week: 'saturday',
+      time_periods: [{ start_time: '00:00', end_time: '23:59' }],
+    },
+    {
+      day_of_week: 'sunday',
+      time_periods: [{ start_time: '00:00', end_time: '23:59' }],
+    },
+  ],
+  category_ids: [],
 })
 
 db.createCollection('takes')
